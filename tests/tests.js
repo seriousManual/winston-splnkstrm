28abb5b8896915be3f1b06ee20429793896598ca
zaphod1984 <zaphod84@gmx.de>
2013-12-09 06:17:52 +0100

    tests enhanced

--- tests/tests.js
+++ tests/tests.js
@@ -1,14 +1,18 @@
 var expect = require('chai').expect;
+var sandboxed = require('sandboxed-module');
+var sinon = require('sinon');
 
 var splnkstrm = require('../');
 
+function getSplunkstormMock() {
+    return {
+        Log: sinon.spy(function(apiKey, projectId) {
+
+        })
+    };
+}
+
 describe('splnkstrm', function() {
-    var s;
-
-    before(function() {
-
-    });
-
     it('should throw if no apiKey is set', function() {
         expect(function() {
             new splnkstrm({projectId: 'foo'});
@@ -20,5 +24,22 @@ describe('splnkstrm', function() {
             new splnkstrm({apiKey: 'foo'});
         }).to.throw();
     });
+
+    it('sdf', function() {
+        var s = getSplunkstormMock();
+
+        var Splnkstrm = sandboxed.require('../index', {
+            requires: {
+                splunkstorm: s
+            }
+        });
+
+        var a = new Splnkstrm({
+            projectId: 'projectId',
+            apiKey: 'apiKey'
+        });
+
+        expect(s.Log.args).to.deep.equal([['apiKey', 'projectId']]);
+    })
 
 });
